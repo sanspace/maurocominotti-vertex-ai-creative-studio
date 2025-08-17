e15695b4c52a26817e3d0498ae9c7a7b080780c3
maurocominotti <maurocominotti@google.com>
2025-08-17 02:06:47 +0000

    feat: add photoswipe logic for images on media-lightbox module

--- frontend/src/app/common/components/media-lightbox/media-lightbox.component.ts
+++ frontend/src/app/common/components/media-lightbox/media-lightbox.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { MediaItem } from '../../models/media-item.model';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
+import {MediaItem} from '../../models/media-item.model';
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
 
 @Component({
   selector: 'app-media-lightbox',
   templateUrl: './media-lightbox.component.html',
   styleUrls: ['./media-lightbox.component.scss'],
 })
-export class MediaLightboxComponent implements OnChanges {
+export class MediaLightboxComponent
+  implements OnChanges, AfterViewInit, OnDestroy
+{
   @Input() mediaItem: MediaItem | undefined;
   @Input() initialIndex = 0;
 
@@ -17,9 +28,24 @@ export class MediaLightboxComponent implements OnChanges {
   imageWidth = 1920; // A sensible default max width
   imageHeight = 1920;
 
+  private lightbox: PhotoSwipeLightbox | undefined;
+
+  ngAfterViewInit(): void {
+    this.initializePhotoSwipe();
+  }
+
+  ngOnDestroy(): void {
+    this.lightbox?.destroy();
+    this.lightbox = undefined;
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['mediaItem'] || changes['initialIndex']) {
       this.initialize();
+      if (this.lightbox) {
+        this.lightbox.destroy();
+        this.initializePhotoSwipe();
+      }
     }
   }
 
@@ -37,11 +63,35 @@ export class MediaLightboxComponent implements OnChanges {
     }
   }
 
+  private initializePhotoSwipe(): void {
+    if (this.mediaItem?.presignedUrls && !this.isVideo) {
+      this.lightbox = new PhotoSwipeLightbox({
+        dataSource: this.mediaItem.presignedUrls.map(url => ({
+          src: url,
+          width: this.imageWidth,
+          height: this.imageHeight,
+          alt: this.mediaItem?.originalPrompt,
+        })),
+        pswpModule: () => import('photoswipe'),
+      });
+      this.lightbox.init();
+    } else {
+      this.lightbox?.destroy();
+      this.lightbox = undefined;
+    }
+  }
+
   private updateImageDimensions(): void {
     if (this.mediaItem) {
       const aspectRatioStr = this.mediaItem.aspectRatio || '1:1';
       const [w, h] = aspectRatioStr.split(':').map(Number);
       this.imageHeight = (h / w) * this.imageWidth;
+    }
+  }
+
+  openPhotoSwipe(index: number): void {
+    if (this.lightbox) {
+      this.lightbox.loadAndOpen(index);
     }
   }
 
